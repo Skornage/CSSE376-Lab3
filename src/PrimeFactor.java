dee1cca4e8ecf1d6fe28521e54601940a50e1a7d
Skornage <millerna@rose-hulman.edu>
2015-03-24 15:16:38 -0400

    Passed third test case

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -7,7 +7,7 @@ public class PrimeFactor {
 	public static List<Integer> getPrimeFactors(int value) {
 		List<Integer> primes = new ArrayList();
 		if (value > 1) {
-			primes.add(2);
+			primes.add(value);
 		}
 		return primes;
 	}

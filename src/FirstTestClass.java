52e53e0e4f7333447530fad2a71d1d9059d15d8f
Skornage <millerna@rose-hulman.edu>
2015-03-24 15:11:35 -0400

    Created first test case. Failed

--- src/FirstTestClass.java
+++ src/FirstTestClass.java
@@ -1,13 +1,20 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 
 public class FirstTestClass {
 
+	private List<Integer> list(Integer...integers) {
+		return Arrays.asList(integers);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1Empty() {
+		assertEquals(list(), PrimeFactor.getPrimeFactors(1));
 	}
 
 }
---
+++ src/PrimeFactor.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PrimeFactor {
+	
+	public static List<Integer> getPrimeFactors(int value) {
+		return null;
+	}
+
+}

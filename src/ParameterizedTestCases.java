74c03522bb82fc84658b8184b653529186a7bab1
Skornage <millerna@rose-hulman.edu>
2015-03-24 16:00:31 -0400

    Passed first test case

---
+++ src/ParameterizedTestCases.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Collection;
+ 
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ParameterizedTestCases {
+   private Integer inputNumber;
+   private List<Integer> expectedResult;
+
+   public ParameterizedTestCases(Integer inputNumber, 
+      List<Integer> expectedResult) {
+      this.inputNumber = inputNumber;
+      this.expectedResult = expectedResult;
+   }
+   
+   @Parameterized.Parameters
+   public static Collection primeNumbers() {
+      return Arrays.asList(new Object[][] {
+         { 1, list() }
+      });
+   }
+
+   @Test
+   public void testPrimeNumberChecker() {
+      System.out.println("Parameterized Number is : " + inputNumber);
+      assertEquals(expectedResult, PrimeNumberGenerator.generate(inputNumber));
+   }
+   
+   private static List<Integer> list(Integer...integers) {
+		return Arrays.asList(integers);
+	}
+   
+}

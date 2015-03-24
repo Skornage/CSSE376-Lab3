c04475af13180b75c3f2493d0f7023a5e68e9d2a
Skornage <millerna@rose-hulman.edu>
2015-03-24 16:34:51 -0400

    Refactored to make code smaller

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -15,8 +15,7 @@ public class PrimeNumberGenerator {
 
 	private static boolean isPrime(int num) {
 		for (int i = 2; i < num; i++) {
-			if (num % i == 0)
-				return false;
+			if (num % i == 0) return false;
 		}
 		return true;
 	}

2510f279a106296d7b32bd7c5b237c708ae200fb
Skornage <millerna@rose-hulman.edu>
2015-03-24 15:31:52 -0400

    Refactored code to look nice. Still passes test cases

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -6,16 +6,15 @@ public class PrimeFactor {
 	public static List<Integer> getPrimeFactors(int value) {
 		List<Integer> primes = new ArrayList();
 		int check = 2;
-		int i = value;
-		while (i > 1) {
-			for (; i % check == 0; i = i / check) {
+		while (value > 1) {
+			for (; value % check == 0; value /= check) {
 				primes.add(check);
 			}
 			check++;
 		}
 		
-		if (i > 1) {
-			primes.add(i);
+		if (value > 1) {
+			primes.add(value);
 		}
 		return primes;
 	}

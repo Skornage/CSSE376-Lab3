08c96db58a01dda7b8d1b9ed0fff32edb3ff6d85
Skornage <millerna@rose-hulman.edu>
2015-03-24 15:14:55 -0400

    Passed second test case

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class PrimeFactor {
 	
 	public static List<Integer> getPrimeFactors(int value) {
-		return new ArrayList();
+		List<Integer> primes = new ArrayList();
+		if (value > 1) {
+			primes.add(2);
+		}
+		return primes;
 	}
 
 }

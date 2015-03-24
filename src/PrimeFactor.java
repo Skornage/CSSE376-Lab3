6a76f40de031cb4b94fa200f09c7cb1e8bedfb54
Skornage <millerna@rose-hulman.edu>
2015-03-24 15:29:50 -0400

    Passed test case for 9

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class PrimeFactor {
-	
+
 	public static List<Integer> getPrimeFactors(int value) {
 		List<Integer> primes = new ArrayList();
-		int i;
-		for (i = value; i % 2 == 0; i = i / 2) {
-			primes.add(2);
+		int check = 2;
+		int i = value;
+		while (i > 1) {
+			for (; i % check == 0; i = i / check) {
+				primes.add(check);
+			}
+			check++;
 		}
+		
 		if (i > 1) {
 			primes.add(i);
 		}
